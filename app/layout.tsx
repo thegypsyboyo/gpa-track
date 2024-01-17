ad272ac6d09d8508ef74b27d5b212e420e59d11d
thegypsyboyo <lewisonyango9@gmail.com>
2024-01-17 19:31:10 +0300

    updated a clean layout for the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,9 @@
-/* eslint-disable camelcase */
-import React from 'react';
-import { ClerkProvider } from '@clerk/nextjs';
-import { Inter, Space_Grotesk } from 'next/font/google';
-import type { Metadata } from 'next';
+import React from 'react'
 
-import './globals.css';
-import '../styles/prism.css';
-import { ThemeProvider } from '@/context/ThemeProvider';
-
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter'
-});
-const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spaceGrotesk'
-});
-
-export const metadata: Metadata = {
-  title: 'DevOverflow',
-  description:
-    'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structure and more.',
-  icons: '/assets/images/site-logo.svg',
-  openGraph: {
-    images: '/assets/meta.jpeg'
-  }
-};
-
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+const Layout = ({children}: {children:React.ReactNode}) => {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-        <ClerkProvider
-          appearance={{
-            elements: {
-              formButtonPrimary: 'primary-gradient',
-              footerActionLink:
-                'primary-text-gradient hover:text-primary-500'
-            }
-          }}
-        >
-          <ThemeProvider>{children}</ThemeProvider>
-        </ClerkProvider>
-      </body>
-    </html>
-  );
+    <div>{children}</div>
+  )
 }
+
+export default Layout
